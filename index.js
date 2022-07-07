172cccdb53da1a151afb1b397ba698a4f8d65a48
tanvirmohammadrizvy <tanvirmohammad2@gmail.com>
2022-07-07 11:43:31 +0600

    updated

---
+++ index.js
@@ -0,0 +1,15 @@
+/* 
+    Installation:
+        1. npm init
+        2. yarn add nodemon
+        3. yarn add express mongoose dotenv
+        4. yarn add body-parser cors express-fileupload morgan lodash 
+*/
+
+import express from 'express'; // Importing express module
+
+
+const app = express(); // Application configuration
+app.listen('8888', ()=> {
+    console.log('Backend Running');
+})
